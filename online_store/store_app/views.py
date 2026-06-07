5266672b3a1f155f2011eb08361a39c40249e51d
Neo <mrrneo@outlook.com>
2026-06-07 06:20:29 +0600

    ...

--- online_store/store_app/views.py
+++ online_store/store_app/views.py
@@ -1,29 +1,33 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from rest_framework.reverse import reverse_lazy # башкы бетке кайтканга
+from django.urls import reverse_lazy
 from .models import Product
 from .forms import ProductForm
 
-# Create your views here.
+
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'product_list.html'
-    context_object_name = 'list_product_view'
+    context_object_name = 'products'
+
 
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = 'product_detail.html'
-    context_object_name = 'product_detail_view'
+    context_object_name = 'product'
+
 
 class ProductCreateView(CreateView):
     template_name = 'product_create.html'
-    form_class = ProductForm # ушу класстын формасын колдон
-    success_url = reverse_lazy('product_list_url') # тузгондон кийин башкы бетке чыкканга
+    form_class = ProductForm
+    success_url = reverse_lazy('product_list_url')
+
 
 class ProductUpdateView(UpdateView):
     queryset = Product.objects.all()
     template_name = 'product_update.html'
     form_class = ProductForm
     success_url = reverse_lazy('product_list_url')
+
 
 class ProductDeleteView(DeleteView):
     queryset = Product.objects.all()
